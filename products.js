6584bcc557f4ff2bf1b3b58b1361c64209da0057
Adrian Martushev <adrianmartushev@gmail.com>
2024-03-31 15:22:38 -0700

    products

--- products.js
+++ products.js
@@ -1,22 +1,35 @@
 
 
+
 const addProductButton = document.getElementById('add-product-button')
 const createProductModal = document.getElementById('create-product-modal')
 const closeProductModal = document.getElementById('close-product-modal')
+const closeDeleteModal = document.getElementById('close-delete-modal')
+const productsContainer = document.getElementById('products-container'); 
+const productPhotoContainer = document.getElementById('product-photo-container')
+
 
 addProductButton.addEventListener('click', () => {
-    createProductModal.style.display = 'flex'
-})
+    $("#create-product-button").text("Create Product");
+
+    $("#create-product-modal").css('display', 'flex').css('opacity', '0');
+    $("#create-product-modal").fadeTo(400, 1); 
+});
 
 closeProductModal.addEventListener('click', () => {
-    createProductModal.style.display = 'none'
-})
+    $("#create-product-modal").fadeTo(400, 0, function() {
+        $(this).css('display', 'none');
+    });
+});
+
+closeDeleteModal.addEventListener('click', () => {
+    $("#delete-product-modal").fadeTo(400, 0, function() {
+        $(this).css('display', 'none');
+    });
+});
 
 
 async function fetchAllProductsAndBuildBlocks() {
-    const productsContainer = document.getElementById('products-container'); // Ensure you have this in your HTML
-
-    // Clear existing products display
     while (productsContainer.firstChild) {
         productsContainer.removeChild(productsContainer.firstChild);
     }
@@ -25,7 +38,7 @@ async function fetchAllProductsAndBuildBlocks() {
         const productsSnapshot = await db.collection('products').get();
         for (let doc of productsSnapshot.docs) {
             const product = doc.data();
-            buildProductBlock(doc.id, product.title, product.image, product.status, product.pricePerUnit, product.description);
+            buildProductBlock(doc.id, product.title, product.image, product.status, product.price, product.description);
         }
     } catch (error) {
         console.error("Error fetching products: ", error);
@@ -33,37 +46,131 @@ async function fetchAllProductsAndBuildBlocks() {
 }
 
 function buildProductBlock(productId, title, image, status, pricePerUnit, description) {
-    let productBlock = document.createElement('div');
-    productBlock.className = 'row-container';
+    let productRowContainer = createDOMElement('div', 'row-container', '', productsContainer)
 
-    // Image
-    let imgBlock = createDOMElement('img', 'product-image', image, productBlock);
-    imgBlock.src = image; // Assuming 'image' is a path to the image
-
-    // Title
-    createDOMElement('div', 'text-div-20 text-light', title, productBlock);
-
-    // Price per unit
-    createDOMElement('div', 'text-light', `$${pricePerUnit}`, productBlock);
-
-    // Description
-    createDOMElement('div', 'text-light', description, productBlock);
+    // Image + Title
+    let productTitleBlock = createDOMElement('div', 'text-div-20', '', productRowContainer);
+    createDOMElement('img', 'product-image', image, productTitleBlock);
+    createDOMElement('div', 'text-light', title, productTitleBlock);
 
     // Status
-    createDOMElement('div', 'text-light', status, productBlock);
+    let statusBlock = createDOMElement('div', 'text-div-20', '', productRowContainer);
+    createDOMElement('div', 'text-light', status, statusBlock);
+
+    // Price per unit
+    let priceBlock = createDOMElement('div', 'text-div-20', '', productRowContainer);
+    createDOMElement('div', 'text-light', `$${pricePerUnit}`, priceBlock);
+
+    // Description
+    let descriptionBlock = createDOMElement('div', 'product-description-block', '', productRowContainer);
+    createDOMElement('div', 'description-text', description, descriptionBlock);
+
+    //Actions
+    let actionsBlock = createDOMElement('div', 'actions-div', '', productRowContainer);
+    let deleteButton = createDOMElement('div', 'action-delete', '', actionsBlock);
+    let editButton = createDOMElement('div', 'action-edit', '', actionsBlock);
+
+    deleteButton.setAttribute('data-product-id', productId); 
+    editButton.setAttribute('data-product-id', productId); 
+
+    // Delete button event listener
+    deleteButton.addEventListener('click', function() {
+        const prodId = this.getAttribute('data-product-id');
+        openDeleteModal(prodId);
+    });
+
+    // Edit button event listener
+    editButton.addEventListener('click', function() {
+        const prodId = this.getAttribute('data-product-id');
+        openEditModal(prodId);
+    });
 
     // Append the productBlock to the container
-    document.getElementById('products-container').appendChild(productBlock);
+    document.getElementById('products-container').appendChild(productRowContainer);
 }
 
 fetchAllProductsAndBuildBlocks()
 
+//Modals
+let editingProductId = null; // Null when creating a new product, holds ID when editing
 
 
-const productPhotoContainer = document.getElementById('product-photo-container')
+// Function to open the delete modal and set up the confirmation action
+async function openDeleteModal(productId) {
 
-document.getElementById('product-photo-button').addEventListener('click', function() {
-    document.getElementById('hidden-file-input').click();
+    try {
+        const doc = await db.collection('products').doc(productId).get();
+        if (doc.exists) {
+            const product = doc.data();
+            $('#delete-product-title').text(product.title);
+            $('#delete-product-description').text(product.description);
+            $("#delete-product-modal").css('display', 'flex').css('opacity', '0');
+            let deleteProductDiv = document.getElementById('delete-product-div')
+            while(deleteProductDiv.firstChild) { 
+                deleteProductDiv.removeChild(deleteProductDiv.firstChild)
+            }
+            createDOMElement('img', 'create-product-image', product.image, deleteProductDiv)
+
+
+            $("#delete-product-modal").fadeTo(400, 1); // Fade in to opacity 1 over 400ms
+
+            // Ensure this doesn't add multiple listeners to the same button over time
+            $("#delete-product-button").off('click').on('click', function() {
+                deleteProduct(productId);
+            });
+        }
+    } catch (error) {
+        console.error('Error loading product for editing: ', error);
+        alert('Error loading product details.');
+    }
+}
+
+async function deleteProduct(productId) {
+    try {
+        await db.collection('products').doc(productId).delete();
+        $("#delete-product-modal").fadeTo(400, 0, function() {
+            $(this).css('display', 'none');
+        });
+        alert('Product deleted successfully');
+        fetchAllProductsAndBuildBlocks()
+    } catch (error) {
+        console.error('Error deleting product: ', error);
+        alert('Error deleting product.');
+    }
+}
+
+
+async function openEditModal(productId) {
+    try {
+        const doc = await db.collection('products').doc(productId).get();
+        if (doc.exists) {
+            editingProductId = productId; 
+            const product = doc.data();
+            $('#product-title').val(product.title);
+            $('#product-description').val(product.description);
+            $('#product-price').val(product.price);
+            document.getElementById('product-photo-button').style.display = 'none'
+
+            while (productPhotoContainer.firstChild) {
+                productPhotoContainer.removeChild(productPhotoContainer.firstChild)
+            }
+            createDOMElement('img', 'create-product-photo', product.image, productPhotoContainer)
+            $("#create-product-button").text("Save Product"); // Change button text for editing
+            $("#create-product-modal").css('display', 'flex').css('opacity', '0');
+            $("#create-product-modal").fadeTo(400, 1);
+        }
+    } catch (error) {
+        console.error('Error loading product for editing: ', error);
+        alert('Error loading product details.');
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('product-photo-button').addEventListener('click', function() {
+        document.getElementById('hidden-file-input').click();
+    });
 });
 
 // Handle file selection
@@ -76,6 +183,8 @@ document.getElementById('hidden-file-input').addEventListener('change', function
                 productPhotoContainer.removeChild(productPhotoContainer.firstChild)
             }
             createDOMElement('img', 'create-product-image', fileLoadEvent.target.result, productPhotoContainer)
+            document.getElementById('product-photo-button').style.display = 'none'
+
         };
         fileReader.readAsDataURL(event.target.files[0]); // Convert the file to a data URL
     }
@@ -83,34 +192,74 @@ document.getElementById('hidden-file-input').addEventListener('change', function
 
 
 document.getElementById('create-product-button').addEventListener('click', async function() {
-    const fileInput = document.getElementById('product-photo-button');
+    const fileInput = document.getElementById('hidden-file-input');
     const titleInput = document.getElementById('product-title');
     const descriptionInput = document.getElementById('product-description');
     const priceInput = document.getElementById('product-price');
 
+    // Check if a new image was uploaded; if not, keep the existing one
     if (fileInput.files.length > 0) {
         const file = fileInput.files[0];
         const storageRef = firebase.storage().ref('products/' + file.name);
-        const uploadTask = storageRef.put(file);
-
         try {
-            // Wait for the file to upload
-            await uploadTask;
-            // Get URL of the uploaded file
-            const photoURL = await storageRef.getDownloadURL();
-            
-            // Now save the product data to Firestore
-            await db.collection('products').add({
+            const uploadTask = await storageRef.put(file);
+            const photoURL = await uploadTask.ref.getDownloadURL();
+
+            saveOrUpdateProduct({
                 title: titleInput.value,
                 description: descriptionInput.value,
                 price: parseFloat(priceInput.value),
-                image: photoURL
+                image: photoURL,
+                status: 'available'
             });
-
-            alert('Product saved successfully!');
         } catch (error) {
             console.error('Error saving product: ', error);
             alert('Error saving product.');
         }
+    } else if (editingProductId) { // No new image, updating existing product
+        saveOrUpdateProduct({
+            title: titleInput.value,
+            description: descriptionInput.value,
+            price: parseFloat(priceInput.value),
+            // Image not updated, assuming existing URL is kept
+            status: 'available'
+        }, editingProductId);
+    } else {
+        alert("Please select an image for the product.");
     }
 });
+
+
+async function saveOrUpdateProduct(productData, productId = null) {
+    if (productId) {
+        // Update existing product
+        await db.collection('products').doc(productId).update(productData);
+        alert('Product updated successfully!');
+    } else {
+        // Add new product
+        await db.collection('products').add(productData);
+        alert('Product added successfully!');
+    }
+    fetchAllProductsAndBuildBlocks();
+    $("#create-product-modal").fadeTo(400, 0, function() {
+        $(this).css('display', 'none');
+    });
+    // Reset global variable and form after operation
+    editingProductId = null;
+    resetForm();
+}
+
+
+
+function resetForm() {
+    // Reset form fields and the file input if necessary
+    $('#product-title').val('');
+    $('#product-description').val('');
+    $('#product-price').val('');
+    $('#hidden-file-input').val('');
+    document.getElementById('product-photo-button').style.display = 'flex'
+    // Remove the displayed image
+    while (productPhotoContainer.firstChild) {
+        productPhotoContainer.removeChild(productPhotoContainer.firstChild);
+    }
+}
